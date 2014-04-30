e540c6e3ebd52d613deca75434f9cadbcbec2628
unknown <x2ns@W0310736.nordstrom.net>
2014-04-30 13:19:00 -0600

    First Commit

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,54 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private static readonly IContactRepository _contacts = new ContactRepository();
+
+        public IQueryable<Contact> Get()
+        {
+            return _contacts.GetAllContacts().AsQueryable();
+        }
+
+        ///GET - api/contact/{id}
+        public Contact Get(string id)
+        {
+            Contact contact = _contacts.GetContact(id);
+            if (contact == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return contact;
+        }
+
+        public Contact Post(Contact person)
+        {
+            Contact contact = _contacts.AddContact(person);
+            return contact;
+        }
+
+        public void Put(string id, Contact value)
+        {
+            if (!_contacts.UpdateContact(id, value))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        public void Delete(string id)
+        {
+            if (!_contacts.RemoveContact(id))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
---
+++ Begin/ContactManager/Models/IContactRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Models
+{
+    public interface IContactRepository
+    {
+        IEnumerable<Contact> GetAllContacts();
+
+        Contact GetContact(string id);
+
+        Contact AddContact(Contact item);
+
+        bool RemoveContact(string id);
+
+        bool UpdateContact(string id, Contact item);
+    }
+}
